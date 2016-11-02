18691a153754b3b75f93beb9fb2d45c48964078d
hbayyavarapu <whimsical1987@gmail.com>
2016-11-01 22:17:22 -0400

    Initial commit

---
+++ src/algorithms/search/BinarySearch.java
@@ -0,0 +1,7 @@
+package algorithms.search;
+
+/**
+ * Created by Harish on 11/1/16.
+ */
+public class BinarySearch {
+}
---
+++ src/algorithms/sorting/QuickSort.java
@@ -0,0 +1,42 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+/**
+ * Created by Harish on 11/1/16.
+ */
+public class QuickSort {
+    public static void main(String [] args) {
+        int[] arr = {12, 9, 7, 15, 10};
+        int start = 0;
+        int end = arr.length -1;
+        quickSort(arr, start, end);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if(start < end) {
+            int pivot = partition(arr, start, end);
+            quickSort(arr, start, pivot - 1);
+            quickSort(arr, pivot + 1, end);
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = start;
+
+        for(int j = start; j < end-1; j++) {
+            if(arr[j] <= pivot) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+
+        }
+        arr[end] = arr[i];
+        arr[i] = pivot;
+        return i;
+    }
+}
